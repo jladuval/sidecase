0ec2d992266092b49dff1d9d259da58d07ba0e71
jladuval <jladuval@gmail.com>
2014-04-02 22:57:53 +1300

    angular is working nicely. text is good as well

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,8 @@
-function SidecaseCtrl($scope){
+function SidecaseCtrl($scope, $timeout){
   $scope.projects = [
     {name: 'project1'},
     {name: 'project2'}
   ];
+  $timeout(function(){
+    $scope.projects.push({name: 'project3'});}, 3000);
 }
